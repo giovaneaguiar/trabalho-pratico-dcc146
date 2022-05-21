187d66a54aa7c372d527943b4258956afb58aa38
Augusto Castilho <castilho@ice.ufjf.br>
2022-05-21 18:30:36 -0300

    Menu de opções

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("\n\033[1mAspectos Teóricos da Computação(DCC146) - Trabalho Prático\033[0m\n")
+print("Para utilização do programa, utilize as seguintes instruções:")
+print("\t\033[1m:d\033[0m - realiza a divisão em tags da string do arquivo informado")
+print("\t\033[1m:c\033[0m - carrega um arquivo com definições de tags")
+print("\t\033[1m:o\033[0m - especifica o caminho do arquivo de saída para a divisão em tags")
+print("\t\033[1m:p\033[0m - realiza a divisão em tags da entrada informada")
+print("\t\033[1m:a\033[0m - Lista as definições formais dos autômatos em memória")
+print("\t\033[1m:l\033[0m - Lista as definições de tag válidas")
+print("\t\033[1m:q\033[0m - sair do programa")
+print("\t\033[1m:s\033[0m - salvar as tags")
