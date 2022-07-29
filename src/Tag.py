f0733039b0c4f1b72033c8b3f182620487658131
Augusto Castilho <castilho@ice.ufjf.br>
2022-07-29 15:45:40 -0300

    Update Validação das Tags

--- src/Tag.py
+++ src/Tag.py
@@ -3,10 +3,13 @@
 # Matheus Rubio - 201876036
 from src.MessageLogs import MessageLogs
 
+stack = []
+
 
 class Tag:
-    def  __init__(self, tagName, tagValue):
-        self.tagName =  tagName
+
+    def __init__(self, tagName, tagValue):
+        self.tagName = tagName
         self.tagValue = tagValue
 
     @staticmethod
@@ -14,8 +17,51 @@ class Tag:
         tagName = tag[0].upper()
         tagValue = tag[1]
 
-        if ' ' in tagName or ' ' in tagValue:
-            MessageLogs.error(f'Formato da tag inválido!')
+        if ' ' in tagName:
             return False
 
+        while len(tagValue) > 0:
+            match tagValue[0]:
+                case '+':
+                    if len(stack) >= 2:
+                        lastValue = stack.pop(len(stack) - 1)
+                        penultValue = stack.pop(len(stack) - 1)
+                        newValue = penultValue + "+" + lastValue
+                        stack.append(newValue)
+                        tagValue = tagValue[1:len(tagValue)]
+                    else:
+                        stack.clear()
+                        return False
+                case '.':
+                    if len(stack) >= 2:
+                        lastValue = stack.pop(len(stack) - 1)
+                        penultValue = stack.pop(len(stack) - 1)
+                        newValue = penultValue + lastValue
+                        stack.append(newValue)
+                        tagValue = tagValue[1:len(tagValue)]
+                    else:
+                        stack.clear()
+                        return False
+                case '*':
+                    if len(stack) >= 1:
+                        lastValue = stack.pop(len(stack) - 1)
+                        newValue = "(" + lastValue + ")*"
+                        stack.append(newValue)
+                        tagValue = tagValue[1:len(tagValue)]
+                    else:
+                        stack.clear()
+                        return False
+                case ' ':
+                    return True
+                case default:
+                    stack.append(tagValue[0])
+                    tagValue = tagValue[1:len(tagValue)]
+
+        print("PILHA FINAL: " + stack[0])
+
+        if len(stack) != 1:
+            stack.clear()
+            return False
+
+        stack.clear()
         return True
