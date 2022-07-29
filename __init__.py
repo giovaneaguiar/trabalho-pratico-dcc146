fa7dd92535c9942e84265cc3a87e1b3880924e53
Augusto Castilho <castilho@ice.ufjf.br>
2022-07-29 15:48:22 -0300

    Remoção do menu

--- __init__.py
+++ __init__.py
@@ -6,18 +6,6 @@ from src.Commands import Commands
 from src.MessageLogs import MessageLogs
 from src.Tag import Tag
 
-def menu():
-    MessageLogs.info("Para utilização do programa, utilize as seguintes instruções ou defina uma TAG:")
-    print("\t\033[1m:d\033[0m - realiza a divisão em tags da string do arquivo informado")
-    print("\t\033[1m:c\033[0m - carrega um arquivo com definições de tags")
-    print("\t\033[1m:o\033[0m - especifica o caminho do arquivo de saída para a divisão em tags")
-    print("\t\033[1m:p\033[0m - realiza a divisão em tags da entrada informada")
-    print("\t\033[1m:a\033[0m - Lista as definições formais dos autômatos em memória")
-    print("\t\033[1m:l\033[0m - Lista as definições de tag válidas")
-    print("\t\033[1m:q\033[0m - sair do programa")
-    print("\t\033[1m:s\033[0m - salvar as tags\n")
-    return input("Digite a operação desejada: ")
-
 
 if __name__ == "__main__":
     print("\n\033[1m------Aspectos Teóricos da Computação(DCC146) - Trabalho Prático------\033[0m\n")
@@ -25,7 +13,7 @@ if __name__ == "__main__":
     tags = {}   # Array com as tags criadas durante a execução da aplicação.
 
     while True:
-        userInput = menu()
+        userInput = input()
         optionSelected = userInput.split()[0].upper()  # recebe apenas a primeira palavra passada, que é o comando
 
         match optionSelected:
