ebddad2f2748318723e071b122845f7065b0c722
Augusto Castilho <castilho@ice.ufjf.br>
2022-07-30 15:58:05 -0300

    Correção da validação de Tags
    Agora podem ser inseridos caracteres especiais na linguagem, como +, *, .

--- src/Tag.py
+++ src/Tag.py
@@ -1,7 +1,6 @@
 # Augusto Castilho - 201876044
 # Giovane Machado - 201876019
 # Matheus Rubio - 201876036
-from src.MessageLogs import MessageLogs
 
 stack = []
 
@@ -53,6 +52,18 @@ class Tag:
                         return False
                 case ' ':
                     return True
+
+                # A fim de definir linguagens (ou tags) com caracteres especiais, que usam os meta-símbolos da linguagem
+                # ou a cadeia vazia (λ), serão usados símbolos de escape. Os símbolos de escape são especificados
+                # usando o símbolo \ seguido por um caractere.
+                case '\\':
+                    if tagValue[1] == 'n':  # Se vier um \n é quebra de linha, não sei como representar e nem pra que usar isso
+                        return True
+                    elif tagValue[1] == 'l':  # Se vier um \l é Lambda, não sei como representar e nem pra que usar isso
+                        return True
+                    else:
+                        stack.append(tagValue[1])
+                        tagValue = tagValue[2:len(tagValue)]
                 case default:
                     stack.append(tagValue[0])
                     tagValue = tagValue[1:len(tagValue)]
